a7aac03b57c00b7afdfbc4c23b649062964a30f3
Fernando Angolar <fernandoangolar581@gmail.com>
2025-03-05 16:42:27 +0100

    :tada: init: inicio do projecto

---
+++ src/main/java/ao/com/wundu/ApiFinancesApplication.java
@@ -0,0 +1,13 @@
+package ao.com.wundu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFinancesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFinancesApplication.class, args);
+	}
+
+}
