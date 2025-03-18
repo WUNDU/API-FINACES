f46b3cd9eede726ff40ee188baef2bcaebf099ce
Fabio Kelvin <kelvinfabio2@gmail.com>
2025-03-18 11:43:30 +0100

    🍃 feat(dev): configure prod profile with MongoDB

--- src/main/java/ao/com/wundu/ApiFinancesApplication.java
+++ src/main/java/ao/com/wundu/ApiFinancesApplication.java
@@ -2,7 +2,9 @@ package ao.com.wundu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = "ao.com.wundu.repository")
 @SpringBootApplication
 public class ApiFinancesApplication {
 
--- src/main/java/ao/com/wundu/repository/UserRepository.java
+++ src/main/java/ao/com/wundu/repository/UserRepository.java
@@ -3,8 +3,10 @@ package ao.com.wundu.repository;
 import ao.com.wundu.entity.User;
 //import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import java.util.Optional;
+
 
 public interface UserRepository extends MongoRepository<User, String> {
     boolean existsByEmail(String email);
